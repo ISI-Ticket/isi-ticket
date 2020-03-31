1eabaf34abe3ffdbaae94c0d62af527a36e54891
Eduardo Alexandre Gonçalves Ribeiro <a85422@uminho.pt>
2020-03-31 05:34:12 +0100

    facebook login falta api & butoes responsivos

--- front_end/vendor/js/signin.js
+++ front_end/vendor/js/signin.js
@@ -3,25 +3,25 @@ window.onload = renderButton();
 
 function renderButton() {
     gapi.signin2.render('my-signin2', {
-      'scope': 'profile email',
-      'width': 240,
-      'height': 50,
-      'longtitle': true,
-      'theme': 'dark',
-      'onsuccess': onSignIn,
-      'onfailure': onFailure
+        'scope': 'profile email',
+        //'width': 240,
+        //'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSignIn,
+        'onfailure': onFailure
     });
-  }
+}
 
-  function onFailure(error) {
+function onFailure(error) {
     console.log(error);
-  }
-function signOut(){
+}
+function signOut() {
 
     let auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
-      });
+    });
 }
 
 /*function onSignIn(googleUser){
@@ -42,34 +42,93 @@ function signOut(){
     
 }*/
 
-function onSignIn(googleUser){
+function onSignIn(googleUser) {
 
     profile = googleUser.getBasicProfile();
-    
+
 
     var data = {
-        "email" : profile.getEmail()
+        "email": profile.getEmail()
     }
     console.log(data);
-    fetch('http://localhost:5000/signin',{
-        headers: {'Content-Type': 'application/json'},
+    fetch('http://localhost:5000/signin', {
+        headers: { 'Content-Type': 'application/json' },
         method: 'POST',
         credentials: "include",
         body: JSON.stringify(data)
-        }).then(function(res){ return res.json(); 
-        }).then(function(data){
-            console.log(JSON.stringify(data));
-            let response = JSON.stringify(data);
-            if(data.exists == true) {
-                localStorage.setItem('profile', response);
-                window.location.href='./vendor/pages/perfil.html'
-            }else{
-                localStorage.setItem("email", profile.getEmail());
-                localStorage.setItem("firstname", profile.getGivenName());
-                localStorage.setItem("lastname", profile.getFamilyName());
-            };
-        });
+    }).then(function (res) {
+        return res.json();
+    }).then(function (data) {
+        console.log(JSON.stringify(data));
+        let response = JSON.stringify(data);
+        if (data.exists == true) {
+            localStorage.setItem('profile', response);
+            window.location.href = './vendor/pages/perfil.html'
+        } else {
+            localStorage.setItem("email", profile.getEmail());
+            localStorage.setItem("firstname", profile.getGivenName());
+            localStorage.setItem("lastname", profile.getFamilyName());
+        };
+    });
 
     //window.location.href='./vendor/pages/perfil.html';
-    
+
+}
+
+//Facebook login - ver melhor o logout...
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '253404675657890',
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: 'v6.0'
+    });
+};
+
+// Load the SDK asynchronously
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function facebookLogin() {
+    FB.getLoginStatus((response) => {
+        if (response.status === 'connected') {
+            FB.api('/me?fields=first_name, last_name, email, picture.type(large)', function (userData) {
+                let data = {
+                    "email": userData.email
+                }
+                
+                signInApi(data);
+            });
+        }
+
+    }, { scope: 'public_profile, email' })
+}
+
+function facebookLogout() {
+    FB.logout((response) => {
+        console.log(response);
+    });
+}
+
+async function signInApi(data) {
+    try {
+        let response = await fetch("http://localhost:5000/signin", {
+            headers: { 'Content-Type': 'application/json' },
+            method: 'POST',
+            credentials: "include",
+            body: JSON.stringify(data)
+        });
+
+        if(response.status == 200) {
+            
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
 }
