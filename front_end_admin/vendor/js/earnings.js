9fda35ef99b54cc0d9288e5a6a10864a27322802
Luis Barboza <luis.manuel.monteiro@hotmail.com>
2020-06-15 00:06:24 +0100

    rrr

--- front_end_admin/vendor/js/earnings.js
+++ front_end_admin/vendor/js/earnings.js
@@ -4,7 +4,6 @@ async function getYearlyEarnings() {
     // let response = await fetch('http://localhost:5000/admin/getYearlyEarnings');
     let response = await fetch('https://isi-ticket-api.herokuapp.com/admin/getYearlyEarnings');
     let data = await response.json()
-    console.log("ANO")
     console.log(data)
     return data;
 }
@@ -26,24 +25,26 @@ async function getDailyEarnings() {
 }
 
 function populate() {
-    console.log("CHEGA")
     let dailyEarnings = document.getElementById("dailyEarnings")
     let monthlyEarnings = document.getElementById("monthlyEarnings")
     let yearlyEarnings = document.getElementById("yearlyEarnings")
 
     this.getDailyEarnings().then(data => {
-        if (data.total != null) dailyEarnings.innerHTML = `<br>Math.round(${data.total})€`;
+        y = Math.round(data.total);
+        if (data.total != null) dailyEarnings.innerHTML = `<br>${y}€`;
         else dailyEarnings.innerHTML = '<br>0€'
     })
 
     this.getMonthlyEarnings().then(data => {
-        if (data.total != null) monthlyEarnings.innerHTML = `<br>${data.total}€`;
+        z = Math.round(data.total);
+        if (data.total != null) monthlyEarnings.innerHTML = `<br>${z}€`;
         else monthlyEarnings.innerHTML = '<br>0€'
     })
 
     this.getYearlyEarnings().then(data => {
-        //    if (data.total != null) yearlyEarnings.innerHTML = `<br>Math.round(${data.total})€`;
-        if (data.total != null) yearlyEarnings.innerHTML = `<br>${data.total}€`;
+        x = Math.round(data.total);
+        if (data.total != null) yearlyEarnings.innerHTML = `<br>${x}€`;
+        //  if (data.total != null) yearlyEarnings.innerHTML = `<br>${data.total}€`;
         else yearlyEarnings.innerHTML = '<br>0€'
     })
 }
